34494972c27edf09ea874bef3412877344812f33
Jesús Alexander Aguirre Julcapoma <aguione@gmail.com>
2014-11-30 18:37:38 -0500

    [14.11.30]-[CO]-[DAO y SERVICE]

---
+++ pack-ws/src/main/java/com/bbva/packws/service/impl/SolicitudServiceImpl.java
@@ -0,0 +1,23 @@
+package com.bbva.packws.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.bbva.packws.dao.SolicitudDAO;
+import com.bbva.packws.domain.Solicitud;
+import com.bbva.packws.service.SolicitudService;
+
+@Service("solicitudService")
+public class SolicitudServiceImpl implements SolicitudService {
+	
+	@Resource(name = "solicitudDAO")
+	private SolicitudDAO solicitudDAO;
+
+	public List<Solicitud> consultarSolicitudes(Solicitud parametro) {
+		return solicitudDAO.consultarSolicitudes(parametro);
+	}
+
+}
