8a65d61a2709f494af7cd47389d97247d815a281
Juan Quedena <juanquedena@gmail.com>
2014-12-26 10:02:10 -0500

    [14.12.26]-[CO]-[Correciónes y mejoras de la arquitectura base]

--- com.everis.web/src/main/java/com/everis/web/controller/impl/AbstractSpringControllerImpl.java
+++ com.everis.web/src/main/java/com/everis/web/controller/impl/AbstractSpringControllerImpl.java
@@ -18,77 +18,78 @@ import flexjson.JSONSerializer;
 
 public abstract class AbstractSpringControllerImpl implements AbstractController, Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private Logger LOG = Logger.getLogger(AbstractSpringControllerImpl.class);
-	
-	public String getMensaje(String key) throws IOException {
-		Properties properties = new Properties();
-		InputStream is = this.getClass().getResourceAsStream("/messages.properties");
-		properties.load(is);
-		return properties.getProperty(key);
-	}
-	
-	public String renderModelJson(Object object) {
-		return renderModelJsonDeepExclude(object, new String[]{"*.class"});
-	}
+    private static final long serialVersionUID = 1L;
+    private Logger LOG = Logger.getLogger(AbstractSpringControllerImpl.class);
 
-	public String renderModelJsonDeepExclude(Object object, String[] exclude) {
-		String json = "";
-		try {
-			json =  new JSONSerializer().exclude(exclude).deepSerialize(object);
-		} catch(Exception e) {
-			LOG.error("GenericController:renderModelJsonDeepExclude", e);
-			StringBuilder sb = new StringBuilder();
-			sb.append("{\"mensaje\": \"");
-			sb.append(TrazaUtil.mostrarMensajeHTML(e));
-			sb.append("\", \"tipoResultado\": \"ERROR_SISTEMA\"}");
-			json = sb.toString();
-		}
-		LOG.info(json);
-		return json;
-	}
+    public String getMensaje(String key) throws IOException {
+        Properties properties = new Properties();
+        InputStream is = this.getClass().getResourceAsStream("/messages.properties");
+        properties.load(is);
+        return properties.getProperty(key);
+    }
 
-	public String renderModelJsonDeepInclude(Object object, String[] include) {
-		String json = "";
-		try {
-			json =  new JSONSerializer().include(include).serialize(object);
-		} catch(Exception e) {
-			LOG.error("GenericController:renderModelJsonDeepExclude", e);
-			StringBuilder sb = new StringBuilder();
-			sb.append("{\"mensaje\": \"");
-			sb.append(TrazaUtil.mostrarMensajeHTML(e));
-			sb.append("\", \"tipoResultado\": \"ERROR_SISTEMA\"}");
-			json = sb.toString();
-		}
-		LOG.info(json);
-		return json;
-	}
+    public String renderModelJson(Object object) {
+        return renderModelJsonDeepExclude(object, new String[] { "*.class" });
+    }
 
-	public String renderErrorSistema(Exception ex) {
-		LOG.error("renderErrorSistema", ex);
-		BaseModel baseModel = new BaseModel();
-		baseModel.setMensaje(TrazaUtil.mostrarMensajeHTML(ex));
-		baseModel.setTipoResultado(Resultado.ERROR_SISTEMA);
-		return this.renderModelJson(baseModel);
-	}
+    public String renderModelJsonDeepExclude(Object object, String[] exclude) {
+        String json = "";
+        try {
+            json = new JSONSerializer().exclude(exclude).deepSerialize(object);
+        } catch (Exception e) {
+            LOG.error("GenericController:renderModelJsonDeepExclude", e);
+            StringBuilder sb = new StringBuilder();
+            sb.append("{\"mensaje\": \"");
+            sb.append(TrazaUtil.mostrarMensajeHTML(e));
+            sb.append("\", \"tipoResultado\": \"ERROR_SISTEMA\"}");
+            json = sb.toString();
+        }
+        LOG.info(json);
+        return json;
+    }
 
-	public String renderErrorSistema(List<ObjectError> ex) {
-		BaseModel baseModel = new BaseModel();
-		
-		StringBuilder sb = new StringBuilder();
-		for(ObjectError e: ex) {
-			LOG.error("renderErrorSistema [ObjectName:" + e.getObjectName() + ", DefaultMessage:" + e.getDefaultMessage() + "]");
-			sb.append("ObjectName: ");
-			sb.append(e.getObjectName());
-			sb.append(" - DefaultMessage: ");
-			sb.append(e.getDefaultMessage());
-			sb.append("<br/>");
-		}
-		
-		baseModel.setMensaje(sb.toString());
-		baseModel.setTipoResultado(Resultado.ERROR_SISTEMA);
-		return this.renderModelJson(baseModel);
-	}
-	
-	public void render(String rendered) throws IOException { }
+    public String renderModelJsonDeepInclude(Object object, String[] include) {
+        String json = "";
+        try {
+            json = new JSONSerializer().include(include).serialize(object);
+        } catch (Exception e) {
+            LOG.error("GenericController:renderModelJsonDeepExclude", e);
+            StringBuilder sb = new StringBuilder();
+            sb.append("{\"mensaje\": \"");
+            sb.append(TrazaUtil.mostrarMensajeHTML(e));
+            sb.append("\", \"tipoResultado\": \"ERROR_SISTEMA\"}");
+            json = sb.toString();
+        }
+        LOG.info(json);
+        return json;
+    }
+
+    public String renderErrorSistema(Exception ex) {
+        LOG.error("renderErrorSistema", ex);
+        BaseModel baseModel = new BaseModel();
+        baseModel.setMensaje(TrazaUtil.mostrarMensajeHTML(ex));
+        baseModel.setTipoResultado(Resultado.ERROR_SISTEMA);
+        return this.renderModelJson(baseModel);
+    }
+
+    public String renderErrorSistema(List<ObjectError> ex) {
+        BaseModel baseModel = new BaseModel();
+
+        StringBuilder sb = new StringBuilder();
+        for (ObjectError e : ex) {
+            LOG.error("renderErrorSistema [ObjectName:" + e.getObjectName() + ", DefaultMessage:" + e.getDefaultMessage() + "]");
+            sb.append("ObjectName: ");
+            sb.append(e.getObjectName());
+            sb.append(" - DefaultMessage: ");
+            sb.append(e.getDefaultMessage());
+            sb.append("<br/>");
+        }
+
+        baseModel.setMensaje(sb.toString());
+        baseModel.setTipoResultado(Resultado.ERROR_SISTEMA);
+        return this.renderModelJson(baseModel);
+    }
+
+    public void render(String rendered) throws IOException {
+    }
 }
